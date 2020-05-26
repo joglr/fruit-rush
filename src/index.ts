e2f772ef8cc45b3390e76bea2355e60f7fdf9caa
Kristoffer Højelse <info@hojelse.com>
2020-05-26 23:55:07 +0200

    Make character movable
    Close #4

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,16 @@ const mapConfig = {
     imageSize: 12
 }
 
-const player = '🐨'
+const playerIcon = '🐨'
 const gameContainer = document.querySelector('#game')
 const infoContainer = document.querySelector('#info')
 let lastAnimationFrameID 
 let gamepadIndex = null
 
-const div = document.createElement('div')
-div.textContent = player
+const playerElement = document.createElement('div')
+playerElement.textContent = playerIcon
 
-gameContainer.appendChild(div)
+gameContainer.appendChild(playerElement)
 
 setInterval(() => {
     if (gamepadIndex !== null) {
@@ -40,8 +40,51 @@ window.addEventListener('gamepaddisconnected', (e : GamepadEvent) => {
 })
 
 function gameLoop() {
-    infoContainer.textContent = navigator.getGamepads()[gamepadIndex].timestamp.toString()
+    // infoContainer.textContent = navigator.getGamepads()[gamepadIndex].timestamp.toString()
 
+    infoContainer.textContent = navigator.getGamepads()[0].axes.toString();
+
+    let leftStickX = navigator.getGamepads()[0].axes[0];
+    let leftStickY = navigator.getGamepads()[0].axes[1];
+    let rightStickX = navigator.getGamepads()[0].axes[2];
+    let rightStickY = navigator.getGamepads()[0].axes[3];
+
+    infoContainer.innerHTML = `
+    <div>leftStickX = ${leftStickX}</div>
+    <div>leftStickY = ${leftStickY}</div>
+    <div>rightStickX = ${rightStickX}</div>
+    <div>rightStickY = ${rightStickY}</div>
+    
+    `
+
+    updateGameState(navigator.getGamepads()[0]);
+
+    render();
+    
     requestAnimationFrame(gameLoop)
+}
+
+// Render
+
+function render() {
+    playerElement.style.transform = `translate(${playerPosition[0]}px,${playerPosition[1]}px)`
+}
+
+// Game state
+
+function updateGameState(gamepad : Gamepad) {
+
+    let leftStickX = navigator.getGamepads()[0].axes[0];
+    let leftStickY = navigator.getGamepads()[0].axes[1];
+    movePlayerRelatively(leftStickX,leftStickY);
+}
+
+let playerPosition : [number,number]
+
+playerPosition = [0,0]
+
+function movePlayerRelatively(dx : number, dy : number) {
+    playerPosition[0] += dx;
+    playerPosition[1] += dy;
 }
 
