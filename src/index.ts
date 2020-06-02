ec26bd3e485e9f1c5a368daa88afbb678729a1fd
Jonas Røssum <jonas.g.roessum@gmail.com>
2020-06-02 14:49:58 +0200

    Add support for multiple players

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,7 @@ import init, {
   GamepadInput,
 } from './modules/InputDevice.js'
 
-init(activateKeyboardPlayer)
-
-function activateKeyboardPlayer() {
-  // inputDevice = new KeyboardInput('d', 'a', 's', 'w')
-  inputDevice = new KeyboardInput(
-    'arrowright',
-    'arrowleft',
-    'arrowdown',
-    'arrowup'
-  )
-}
+init()
 
 const mapConfig = {
   areaSize: 16,
@@ -30,61 +20,136 @@ const playerIcon = '🐨'
 const gameContainer = document.querySelector('#game')
 const infoContainer = document.querySelector('#info')
 let lastAnimationFrameID
-let inputDevice: InputDevice = null
+// let inputDevice: InputDevice
 
-const playerElement = document.createElement('div')
-playerElement.textContent = playerIcon
-
-gameContainer.appendChild(playerElement)
-
+// @ts-ignore
 window.addEventListener('gamepadconnected', (e: GamepadEvent) => {
-  inputDevice = new GamepadInput(e.gamepad.index)
-  lastAnimationFrameID = requestAnimationFrame(gameLoop)
+  console.log('new player')
+  const inputDevice = new GamepadInput(e.gamepad.index)
+  const playerElement = createPlayerElement()
+  //@ts-ignore
+  gameContainer.appendChild(playerElement)
+  players.push(new Player(inputDevice, playerElement))
 })
 
+let keyboardPlayerActive: boolean = false
 window.addEventListener('keydown', () => {
-  activateKeyboardPlayer()
-  lastAnimationFrameID = requestAnimationFrame(gameLoop)
+  if (!keyboardPlayerActive) activateKeyboardPlayer()
+  keyboardPlayerActive = true
 })
 
+// @ts-ignore
 window.addEventListener('gamepaddisconnected', (e: GamepadEvent) => {
-  inputDevice = null
-  cancelAnimationFrame(lastAnimationFrameID)
+  for (const p of players) {
+    if (p.getInputDevice()) {
+      //
+    }
+  }
 })
 
-function gameLoop() {
-  infoContainer.textContent = inputDevice.getMovementVector().toString()
-  updateGameState(inputDevice)
+let lastFrameTime: number
+
+function gameLoop(timeStamp: number) {
+  // @ts-ignore
+  infoContainer.textContent = `fps ${calcFPS(
+    lastFrameTime,
+    timeStamp
+  ).toFixed()}`
+
+  for (const p of players) {
+    //@ts-ignore
+    infoContainer.textContent += `
+    p${players.indexOf(
+      p
+    )}: ${p.getInputDevice().getMovementVector().toString()}`
+  }
+  updateGameState()
   render()
   requestAnimationFrame(gameLoop)
+  lastFrameTime = timeStamp
+}
+
+function calcFPS(lastFrameTime: number, timestamp: number) {
+  return lastFrameTime === undefined ? 0 : 1000 / (timestamp - lastFrameTime)
 }
 
 // Render
 
 function render() {
-  playerElement.style.transform = `translate(${playerPosition[0]}px,${playerPosition[1]}px)`
+  for (const player of players) {
+    const pos = player.getPosition()
+    player.getDOMElement().style.transform = `translate(${pos[0]}px,${pos[1]}px)`
+  }
 }
 
 // Game state
 
-function updateGameState(inputDevice: InputDevice) {
-  const v: [number, number] = inputDevice.getMovementVector()
-  const ball = document.createElement('div')
-  ball.style.transform = `translate(${playerPosition[0]}px,${playerPosition[1]}px)`
-  ball.style.borderRadius = '50%'
-  ball.style.width = '10px'
-  ball.style.height = '10px'
-  ball.style.position = 'absolute'
-  ball.style.backgroundColor = 'white'
-  document.body.appendChild(ball)
-  movePlayerRelatively(v[0], v[1])
+function updateGameState() {
+  // const ball = document.createElement('div')
+  // ball.style.transform = `translate(${playerPosition[0]}px,${playerPosition[1]}px)`
+  // ball.style.borderRadius = '50%'
+  // ball.style.width = '10px'
+  // ball.style.height = '10px'
+  // ball.style.position = 'absolute'
+  // ball.style.backgroundColor = 'white'
+  // document.body.appendChild(ball)
+  for (const player of players) {
+    const pos = player.getPosition()
+    const v: [number, number] = player.getInputDevice().getMovementVector()
+    player.setPosition([pos[0] + v[0], pos[1] + v[1]])
+    // movePlayerRelatively(v[0] * 3, v[1] * 3)
+  }
 }
 
-let playerPosition: [number, number]
+let players: Player[] = []
 
-playerPosition = [0, 0]
+lastAnimationFrameID = requestAnimationFrame(gameLoop)
 
-function movePlayerRelatively(dx: number, dy: number) {
-  playerPosition[0] += dx
-  playerPosition[1] += dy
+class Player {
+  private inputDevice: InputDevice
+  private position: [number, number]
+  private DOMElement: HTMLElement
+
+  constructor(inputDevice: InputDevice, domElement: HTMLElement) {
+    this.inputDevice = inputDevice
+    this.position = [0, 0]
+    this.DOMElement = domElement
+  }
+
+  getInputDevice() {
+    return this.inputDevice
+  }
+
+  getPosition(): [number, number] {
+    return this.position
+  }
+
+  getDOMElement(): HTMLElement {
+    return this.DOMElement
+  }
+
+  setPosition(position: [number, number]) {
+    this.position = position
+  }
+}
+
+function createPlayerElement() {
+  const playerElement = document.createElement('div')
+  playerElement.textContent = playerIcon
+  playerElement.style.display = 'inline-block'
+  return playerElement
+}
+
+function activateKeyboardPlayer() {
+  const inputDevice = new KeyboardInput('d', 'a', 's', 'w')
+  const playerElement = createPlayerElement()
+  //@ts-ignore
+  gameContainer.appendChild(playerElement)
+  players.push(new Player(inputDevice, playerElement))
+  // inputDevice = new KeyboardInput(
+  //   'arrowright',
+  //   'arrowleft',
+  //   'arrowdown',
+  //   'arrowup'
+  // )
 }
