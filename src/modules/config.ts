88eb059c25dc3da89edf3022e6b0ac88f431ac8c
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-09-25 01:19:35 +0200

    Adjust config

--- src/modules/config.ts
+++ src/modules/config.ts
@@ -5,10 +5,10 @@ export const poopVelocity = 5;
 export const poopRecoilMultiplier = 1.4
 
 export const playerJumpAmount = -10;
-export const playerMinHorizontalVelocity = -5;
-export const playerMaxHorizontalVelocity = 5;
+export const playerMinHorizontalVelocity = -7;
+export const playerMaxHorizontalVelocity = 7;
 export const playerMinVerticalVelocity = 0;
 export const playerMaxVerticalVelocity = Infinity
-export const playerTurnStrength = 0.9
+export const playerTurnStrength = 2
 export const playerIndicatorFontSize = 30
 export const playerIndicatorOffset = 0
