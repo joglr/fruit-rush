ab2aef58b7569aa7ec63ae4ba0573b9c13c07705
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-10-18 22:03:49 +0200

    Make displaceable physics framerate independent

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,20 @@
-
-
 export const DEAD_ZONE_THRESHOLD = 0.1
-
 
 export const POSITIONABLE_SIZE = 50
 
-export const pausedText = " (Paused)";
+export const pausedText = " (Paused)"
 
-export const gravityAmount = 0.3;
-export const poopVelocity = 5;
+export const gravityAmount = 0.3
+export const poopVelocity = 5
 export const poopRecoilMultiplier = 2.0
 export const poopGunCoolDown = 800
 export const diarrheaRecoilMultiplier = 0.5
 export const playerStunDuration = 1000
 
-export const playerJumpAmount = -10;
-export const playerMinHorizontalVelocity = -7;
-export const playerMaxHorizontalVelocity = 7;
-export const playerMinVerticalVelocity = 0;
+export const playerJumpAmount = -10
+export const playerMinHorizontalVelocity = -7
+export const playerMaxHorizontalVelocity = 7
+export const playerMinVerticalVelocity = 0
 export const playerMaxVerticalVelocity = Infinity
 export const playerTurnStrength = 1.5
 export const playerIndicatorFontSize = 30
@@ -26,3 +23,5 @@ export const playerInitialLives = 10
 export const fruitMargin = 50
 export const tacoDamage = 1
 export const tacoEffectDuration = 2000
+
+export const FRAMERATE_MIGRATION_DURATION = 1000 / 60
--- src/modules/Icon.ts
+++ src/modules/Icon.ts
@@ -6,7 +6,7 @@ export abstract class Icon extends Displaceable {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   draw(ctx: CanvasRenderingContext2D, _timeStamp: number) {
     // TODO: Find a better way to specify type of Vector2 Generator, so this casting isn't necessary
-    const coords = [...this.p] as unknown as [number, number]
+    const coords = this.getPosition()
     const [, h] = this.dimensions
     ctx.font = `${h}px sans-serif`
 
