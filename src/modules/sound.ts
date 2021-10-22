a8e53ead713d5c67d1f992a6ac84fe6da59bb646
Jonas Røssum <1959615+joglr@users.noreply.github.com>
2021-10-22 15:35:59 +0200

    Preload audio Fixes #31

--- src/modules/sound.ts
+++ src/modules/sound.ts
@@ -1,4 +1,4 @@
-const getSoundPath = (x: string) => `./sounds/${x}.wav`;
+const getSoundPath = (x: string) => `./sounds/${x}.wav`
 // x === "kebab" ? `./sounds/${x}.mp3` :
 const sounds = {
   jump: 0,
@@ -7,18 +7,18 @@ const sounds = {
   eat: 3,
   eat_bad: 4,
   // "kebab": 4
-};
+}
 const audios = Object.keys(sounds).map((s) => {
-  const a = document.createElement("audio");
-  a.src = getSoundPath(s);
-
-  a.volume = 0.5;
+  const a = document.createElement("audio")
+  a.src = getSoundPath(s)
+  a.preload = "auto"
+  a.volume = 0.5
   // (s === "kebab") ? 1 :
   return () => {
-    a.pause();
-    a.currentTime = 0;
-    a.play();
-  };
-});
+    a.pause()
+    a.currentTime = 0
+    a.play()
+  }
+})
 
-export const playSFX = (key: keyof typeof sounds) => audios[sounds[key]]();
+export const playSFX = (key: keyof typeof sounds) => audios[sounds[key]]()
